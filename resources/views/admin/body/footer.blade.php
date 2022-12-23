b07e56c199b2b0b99c47b03ff5eeb21a468c4bc3
Tariqul islam <tnawayn83@yahoo.com>
2022-12-23 15:51:37 +0300

    7.Dashboard Page Segmentation
    Dashboard Page Segmentation

---
+++ resources/views/admin/body/footer.blade.php
@@ -0,0 +1,5 @@
+<strong>Copyright &copy; 2022 <a href="https://Matjel.net">Matjel.net</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.2.0
+    </div>
