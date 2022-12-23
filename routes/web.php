10febecced18aa6740a51924e3df3855fbac4403
Tariqul islam <tnawayn83@yahoo.com>
2022-12-23 14:37:53 +0300

    3.setup admin & user table & Laravel Multi Auth with Breeze 1
    setup admin & user table & Laravel Multi Auth with Breeze 1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -29,3 +31,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+Route::get('admin/dashboard',[AdminController::class,'Admindashboard'])->name('admin.dashboard');
+
